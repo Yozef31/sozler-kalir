39842418f908a0c985f7332bd4e82daa6fc14b8f
Yozef31 <bagizyusuf800@gmail.com>
2026-02-17 12:02:52 +0300

    port fix

--- app.py
+++ app.py
@@ -253,5 +253,8 @@ def admin_panel():
     """
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
